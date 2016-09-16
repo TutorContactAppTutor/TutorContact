871d9158281ed26d812b817f2b00ea04f8864b98
EdwinRobalino <edboroba@gmail.com>
2016-09-16 00:25:21 -0500

    Metodo de buscar usuarios por id

--- app/src/main/java/com/example/drmsoul/appestudiante/RegistroTutor.java
+++ app/src/main/java/com/example/drmsoul/appestudiante/RegistroTutor.java
@@ -1,5 +1,6 @@
 package com.example.drmsoul.appestudiante;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +36,16 @@ public class RegistroTutor extends AppCompatActivity {
     private Button btnRegistrar;
     private TextView txtNombreR, txtApellidoR, txtUsuarioR, txtPassR;
 
+    String ip = "http://logicmathematic.com/sw";
+    String m_insert_usuario = ip+"insert_usuario.php";
+    String m_get_id_usuario = ip+"/get_id_usuario.php";
+    String m_insert_perfil_usuario = ip+"insert_perfil_usuario.php";
+    String m_insert_tutor = ip+"insert_tutor.php";
+    String m_get_id_tutor = ip+"get_id_tutor.php.php";
+    String m_insert_tutor_horario = ip+"get_id_tutor.php.php";
+
+    useService serviceThread;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,14 +56,16 @@ public class RegistroTutor extends AppCompatActivity {
 
         actionBar.setTitle("Registrarse");
 
-        listCarrera.add("¿De qué carrera es tutor?");
-        listCarrera.add("LIPRO");
-        listCarrera.add("LIGRA");
-        listCarrera.add("LICWEB");
-        listCarrera.add("LICCOM");
+        listCarrera.add("Especialidad");
+        listCarrera.add("Tutoria para LIPRO");
+        listCarrera.add("Tutoria para LIGRA");
+        listCarrera.add("Tutoria para LICWEB");
+        listCarrera.add("Tutoria para LICCOM");
 
         spnCarrera = (Spinner)findViewById(R.id.spnCarrera);
         btnRegistrar = (Button)findViewById(R.id.btnRegistro);
+
+
 
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listCarrera) {
             @Override
@@ -96,6 +120,111 @@ public class RegistroTutor extends AppCompatActivity {
         txtUsuarioR = (TextView)findViewById(R.id.txtUsuario);
         txtPassR = (TextView)findViewById(R.id.txtPassword);
 
-        Toast.makeText(getApplicationContext(), txtUsuarioR.getText() + ", su información ha sido guardada.", Toast.LENGTH_SHORT).show();
+        //EMPIEZA PROCESO DE REGISTRO********************************************************
+
+
+        //----------------Guardar usuario----------------------------
+        //serviceThread = new useService();
+        //serviceThread.execute(m_insert_usuario, "1");
+        //--------------Consultar nuevo usuario---------------------
+        serviceThread = new useService();
+        String cont = m_get_id_usuario+"?usuario="+txtUsuarioR.getText()+"&clave="+txtPassR.getText();
+        System.out.println("url enviada "+cont);
+        serviceThread.execute(cont, "2");
+
+
+        //Toast.makeText(getApplicationContext(), txtUsuarioR.getText() + ", su información ha sido guardada. Docente de "+spnCarrera.getSelectedItemPosition(), Toast.LENGTH_SHORT).show();
+    }
+
+    public class useService extends AsyncTask <String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            System.out.println("ingreso al servicioa");
+            String cadena = params[0];
+            URL url = null;
+            String devuelve ="";
+
+            if (params[1] == "1"){  // INSERT USUARIO
+
+            }else if (params[1] == "2"){ // RETURN USUARIO NUEVO
+                try {
+                    url = new URL(cadena);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection(); //Abrir la conexión
+                    connection.setRequestProperty("User-Agent", "Mozilla/5.0" +
+                            " (Linux; Android 1.5; es-ES) Ejemplo HTTP");
+                    //connection.setHeader("content-type", "application/json");
+
+                    int respuesta = connection.getResponseCode();
+                    StringBuilder result = new StringBuilder();
+
+                    if (respuesta == HttpURLConnection.HTTP_OK){
+                        System.out.println("Ingreso aqui");
+
+                        InputStream in = new BufferedInputStream(connection.getInputStream());  // preparo la cadena de entrada
+
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));  // la introduzco en un BufferedReader
+
+                        // El siguiente proceso lo hago porque el JSONOBject necesita un String y tengo
+                        // que tranformar el BufferedReader a String. Esto lo hago a traves de un
+                        // StringBuilder.
+
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            result.append(line);        // Paso toda la entrada al StringBuilder
+                        }
+
+                        //Creamos un objeto JSONObject para poder acceder a los atributos (campos) del objeto.
+                        JSONObject respuestaJSON = new JSONObject(result.toString());   //Creo un JSONObject a partir del StringBuilder pasado a cadena
+                        //Accedemos al vector de resultados
+
+                        String resultJSON = respuestaJSON.getString("estado");   // estado es el nombre del campo en el JSON
+                        System.out.println("EStado= "+resultJSON);
+
+                        if (resultJSON.equals("1")){      // hay un alumno que mostrar
+                            devuelve = devuelve + respuestaJSON.getJSONObject("usuario").getString("id") ;
+                            System.out.println("Respuesta= "+resultJSON);
+                        }
+                        else if (resultJSON.equals("2")){
+                            devuelve = "No existe el Usuario";
+                        }
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                return devuelve;
+
+
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Toast.makeText(getApplicationContext(), "ID encontrada: "+s, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onCancelled(String s) {
+            super.onCancelled(s);
+        }
+
+
     }
 }
